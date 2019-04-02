15f2be19fbeba759a749bfdbaaa6bfb73ff14377
Scott Parlane <sjp@canterburyairpatrol.org>
2019-04-02 10:18:26 +0000

    fix: explicitly declare bash
    Not all systems use bash as sh.
    Also, not all systems automatically
    add wheel to venv when it is created,
    so we need to install that first.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 # Re-create the virtual env
 rm -fr venv
 python3 -m venv venv
 source venv/bin/activate
+pip install wheel
 # Install the required packages
 pip install -r requirements.txt
 
