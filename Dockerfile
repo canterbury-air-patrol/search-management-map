2f8f18515e6e7fb6bd33c8c8eb50341cfbf7dd30
Scott Parlane <scott@parlanenz.com>
2020-04-25 20:11:38 +1200

    docker: make the image stand-alone
    It's still possible to override the code for development,
    but this makes it possible to run the docker image without
    a local copy of the code.
    
    Also, makes it possible to update the database details
    this is mostly useful when starting with docker.

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ ENV PYTHONUNBUFFERED 1
 RUN apt-get update && apt-get install -y libgdal-dev && rm -fr /var/lib/apt/lists/*
 RUN mkdir /code
 WORKDIR /code
-COPY ./requirements.txt /code/
+COPY . /code/
 RUN pip install -r requirements.txt
 RUN pip install gunicorn
+RUN ./setup.sh
 CMD ./docker/app/start.sh
--- setup.sh
+++ setup.sh
@@ -91,6 +91,10 @@ then
         echo "At a minimum you will need to set your postgis parameters"
 fi
 
+[ ! -z "$DB_HOST" ] && sed -i "s|'HOST': .*|'HOST': '$DB_HOST',|" smm/local_settings.py || true
+[ ! -z "$DB_USER" ] && sed -i "s|'USER': .*|'USER': '$DB_USER',|" smm/local_settings.py || true
+[ ! -z "$DB_NAME" ] && sed -i "s|'NAME': .*|'NAME': '$DB_NAME',|" smm/local_settings.py || true
+
 if [ ! -f smm/secretkey.txt ]
 then
 	python -c 'import random; result = "".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)]); print(result)' > smm/secretkey.txt	
