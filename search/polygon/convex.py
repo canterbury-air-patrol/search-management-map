canterbury-air-patrol/search-management-map

commit fcb468d4a14ea80c7d267d33bf0f866a4423b0ad

Resolve or suppress issues reported by flake8+pylint

search/polygon/convex.py
@@ -88,6 +88,7 @@ def decomp(lrng):
     for pt1 in concave_points:
         for pt0 in convex_points:
             if cansee(pt0, pt1, lrng):
+                # pylint: disable=W1114
                 left = sublrng(pt0, pt1, lrng)
                 right = sublrng(pt1, pt0, lrng)
                 tmp = decomp(left) + decomp(right)
@@ -193,12 +194,13 @@ def creep_line(lrng, width):
                         for pnt in i:
                             yield pnt
             except TypeError:
+                # pylint: disable=W0707
                 msg = "{} is of type {}".format(i, type(i))
                 raise TypeError(msg)
 
-    yiter = [y for y in step(ymin, ydist, ymax)]
-    liter = [l for l in stripe(xmin, xmax, yiter)]
-    pts = [p for p in carve(lrng, liter)]
+    yiter = step(ymin, ydist, ymax)
+    liter = stripe(xmin, xmax, yiter)
+    pts = list(carve(lrng, liter))
 
     return LineString(pts)
 
@@ -306,23 +308,23 @@ def perimeter_subarray(pts, pt_arr):
     pts_idx.sort()
 
     # Construct LineString for each segment
-    for count, i1 in enumerate(pts_idx):
-        i0 = pts_idx[count-1]
+    for count, index1 in enumerate(pts_idx):
+        index0 = pts_idx[count-1]
 
         # Yield linestring which wraps over the end
         if count == 0:
-            yield pt_arr[i0:] + pt_arr[:i1+1]
+            yield pt_arr[index0:] + pt_arr[:index1+1]
             continue
 
         # Yield typical linestring segments
-        yield pt_arr[i0:i1+1]
+        yield pt_arr[index0:index1+1]
 
 
 def creep_line_concave(lrng, width):
     """ Return a LineString creeping path across all convex polygons in a
     concave polygon"""
     # Decompose LinearRing into several convex LinearRings
-    lrngs_convex = decomp(LinearRing([pt for pt in lrng]))
+    lrngs_convex = decomp(LinearRing(lrng))
 
     # Create creeping line (LineString) for each convex LinearRing
     creep_lines = [creep_line(lr, width) for lr in lrngs_convex]
@@ -332,9 +334,8 @@ def creep_line_concave(lrng, width):
     creep_line_end_points = [lr[-2] for lr in lrngs_convex]
 
     # Create a pool of end points
-    remaining_points = [pts
-                        for pts in (creep_line_end_points +
-                                    creep_line_start_points)]
+    remaining_points = (creep_line_end_points +
+                                    creep_line_start_points)
 
     # Initialize order of creep lines with the first end point
     i = 0
@@ -360,7 +361,7 @@ def creep_line_concave(lrng, width):
                 if current_segment[1] == pt_arr[0]:
                     found_point = True
                     break
-                elif current_segment[1] == pt_arr[-1]:
+                if current_segment[1] == pt_arr[-1]:
                     pt_arr.reverse()
                     found_point = True
                     break
@@ -373,6 +374,7 @@ def creep_line_concave(lrng, width):
             raise ValueError(msg)
 
         # Add joining line to next creep line
+        # pylint: disable=W0631
         if len(pt_arr) > 1:
             creep_lines_ordered.append(LineString(pt_arr))
 
@@ -398,9 +400,3 @@ def creep_line_concave(lrng, width):
     return LineString([p
                        for l in creep_lines_ordered
                        for p in l])
-
-
-def creep_line_at_angle(lrng, width, angle):
-    """ Returns a LineString creeping path at a set angle
-    across a LinearRing"""
-    pass

assets/forms.py
@@ -16,7 +16,7 @@ class AssetCommandForm(ModelForm):
     """
     def __init__(self, *args, **kwargs):
         self.mission = kwargs.pop('mission')
-        super(AssetCommandForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         mission_assets = MissionAsset.objects.filter(mission=self.mission, removed__isnull=True)
         self.fields['asset'].queryset = Asset.objects.filter(pk__in=[mission_asset.asset.pk for mission_asset in mission_assets])
 

assets/models.py
@@ -68,8 +68,8 @@ class AssetCommand(models.Model):
         ('RON', "Continue"),  # Resume own navigation
         ('CIR', "Circle"),
         ('GOTO', "Goto position"),
-        ('MC', "Mission Complete"), # Return to Base
-        ('AS', "Abandon Search"), # Reassignment
+        ('MC', "Mission Complete"),  # Return to Base
+        ('AS', "Abandon Search"),  # Reassignment
     )
     command = models.CharField(max_length=4, choices=COMMAND_CHOICES)
     REQUIRES_POSITION = ('GOTO',)

assets/test_asset.py
@@ -6,7 +6,7 @@ import json
 
 from django.test import TestCase, Client
 from django.core import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 from .models import Asset, AssetType
 
@@ -21,7 +21,7 @@ class AssetTestCase(TestCase):
         """
         boat = AssetType.objects.create(name='boat')
         wing = AssetType.objects.create(name='wing')
-        user = User.objects.create_user('test', password='password')
+        user = get_user_model().objects.create_user('test', password='password')
         Asset.objects.create(name='PCCR', asset_type=boat)
         Asset.objects.create(name='FX-79-1', asset_type=wing, owner=user)
         Asset.objects.create(name='FX-79-2', asset_type=wing)

assets/test_asset_command.py
@@ -3,7 +3,7 @@ Tests for the asset command class
 """
 
 from django.test import TestCase
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 from .models import Asset, AssetType, AssetCommand
 
@@ -17,7 +17,7 @@ class AssetCommandTestCase(TestCase):
         Create objects for the test
         """
         boat = AssetType.objects.create(name='boat')
-        self.user = User.objects.create_user('test', password='password')
+        self.user = get_user_model().objects.create_user('test', password='password')
         pccr = Asset.objects.create(name='PCCR', asset_type=boat, owner=self.user)
         AssetCommand.objects.create(asset=pccr, issued_by=self.user, command='RON', reason='test')
 

assets/test_asset_type.py
@@ -5,7 +5,7 @@ import json
 
 from django.test import TestCase, Client
 from django.core import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 from .models import AssetType, Asset
 
@@ -21,7 +21,7 @@ class AssetTypeTestCase(TestCase):
         """
         AssetType.objects.create(name='flying-wing', description='RPAS that are only a wing')
         AssetType.objects.create(name='boat', description='Sea-going vessel')
-        User.objects.create_user('test', password='password')
+        get_user_model().objects.create_user('test', password='password')
 
     def test_asset_type_name(self):
         """

check-code.sh
@@ -4,6 +4,6 @@ source venv/bin/activate
 
 pycodestyle --ignore=E501 */*.py
 
-pylint --max-line-length=240 --load-plugins pylint_django map/ data/ assets/ search/ mission/ timeline/ images/
+pylint --max-line-length=240 --load-plugins pylint_django --django-settings-module=smm.settings map/ data/ assets/ search/ mission/ timeline/ images/
 
 deactivate

data/models.py
@@ -112,8 +112,6 @@ class GeoTime(models.Model):
             return True
         return False
 
-
-
     class Meta:
         abstract = True
         indexes = [

commit 3ecd25cc8cedba7f73a3934f8124d7cf0b482d69

search: fix polygon search generation
Fixes: fcb468d4a14ea80c7d267d33bf0f866a4423b0ad

search/polygon/convex.py
@@ -324,7 +324,8 @@ def creep_line_concave(lrng, width):
     """ Return a LineString creeping path across all convex polygons in a
     concave polygon"""
     # Decompose LinearRing into several convex LinearRings
-    lrngs_convex = decomp(LinearRing(lrng))
+    #pylint: disable=R1721
+    lrngs_convex = decomp(LinearRing([pt for pt in lrng]))
 
     # Create creeping line (LineString) for each convex LinearRing
     creep_lines = [creep_line(lr, width) for lr in lrngs_convex]