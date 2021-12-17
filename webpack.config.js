fbbb600e24c7b61c2f9340753fcf37ef493fc20c
Scott Parlane <sjp@canterburyairpatrol.org>
2021-12-17 23:53:47 +1300

    build frontend in production mode

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/static/',
     },
-    mode: 'development',
+    mode: 'production',
     module: {
         rules: [
             {
