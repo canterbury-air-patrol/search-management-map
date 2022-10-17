canterbury-air-patrol/search-management-map

commit 309f33b6569a6cd260aceb34f9a11191f39ebafb

Convert the main page to be dynamically generated in js
Adds some new features:
- Automatically polls for new missions
- Converts times into local time

mission/models.py
@@ -20,6 +20,18 @@ class Mission(models.Model):
     closed = models.DateTimeField(null=True, blank=True)
     closed_by = models.ForeignKey(get_user_model(), on_delete=models.PROTECT, related_name='closer%(app_label)s_%(class)s_related', null=True, blank=True)
 
+    def jsonObject(self, admin):
+        return {
+            'id': self.pk,
+            'name': self.mission_name,
+            'description': self.mission_description,
+            'started': self.started,
+            'creator': self.creator.username,
+            'closed': self.closed,
+            'closed_by': self.closed_by.username if self.closed_by else None,
+            'admin': admin,
+        }
+
 
 class MissionUser(models.Model):
     """

mission/views.py
@@ -89,23 +89,19 @@ def mission_list(request):
     """
     List missions this user can select from.
     """
+    return render(request, 'mission_list.html')
+
+
+@login_required
+def mission_list_data(request):
+    """
+    Provide data for all the missions this user is a member of
+    """
     user_missions = MissionUser.objects.filter(user=request.user)
-    current_missions = []
-    previous_missions = []
-    for user_mission in user_missions:
-        if user_mission.role == 'A':
-            user_mission.mission.is_admin = True
-        if user_mission.mission.closed is not None:
-            previous_missions.append(user_mission.mission)
-        else:
-            current_missions.append(user_mission.mission)
-
     data = {
-        'current_missions': current_missions,
-        'previous_missions': previous_missions,
+        'missions': [ user_mission.mission.jsonObject(user_mission.role == 'A') for user_mission in user_missions ],
     }
-
-    return render(request, 'mission_list.html', data)
+    return JsonResponse(data)
 
 
 @login_required

frontend/mission/list.js
@@ -1,0 +1,176 @@
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import { Table, Button, ButtonGroup } from 'react-bootstrap'
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import * as ReactDOM from 'react-dom/client'
+
+import $ from 'jquery'
+
+class MissionRow extends React.Component {
+  render () {
+    const mission = this.props.mission
+    const buttons = [
+      (<Button key='map' href={`/mission/${mission.id}/map/` }>Map</Button>),
+      (<Button key='details' href={`/mission/${mission.id}/details/` }>Details</Button>),
+      (<Button key='timeline' href={ `/mission/${mission.id}/timeline/`}>Timeline</Button>)
+    ]
+    if (!mission.closed && mission.admin) {
+      buttons.push((<Button key='close' className='btn-danger' href={ `/mission/${mission.id}/close/` }>Close</Button>))
+    }
+    return ((
+      <tr key={mission.id}>
+        <td>{mission.name}</td>
+        <td>{(new Date(mission.started)).toLocaleString()}</td>
+        <td>{mission.creator}</td>
+        <td>
+          <ButtonGroup>
+            { buttons }
+          </ButtonGroup>
+        </td>
+      </tr>))
+  }
+}
+MissionRow.propTypes = {
+  mission: PropTypes.object.isRequired
+}
+
+class ActiveMissionList extends React.Component {
+  render () {
+    const missionRows = []
+    for (const missionIdx in this.props.missions) {
+      const mission = this.props.missions[missionIdx]
+      missionRows.push((
+        <MissionRow
+          key={mission.id}
+          mission={mission} />
+      ))
+    }
+    return (
+      <Table>
+        <thead>
+          <tr key='heading'>
+            <th colSpan={4} align='center'>Active Missions</th>
+          </tr>
+          <tr key='labels'>
+            <th>Mission Name</th>
+            <th>Started</th>
+            <th>By</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          { missionRows }
+        </tbody>
+      </Table>)
+  }
+}
+ActiveMissionList.propTypes = {
+  missions: PropTypes.array.isRequired
+}
+
+class GeneralMissionButtons extends React.Component {
+  render () {
+    return (
+      <div>
+        <Button href='/mission/new/'>Start New Mission</Button>&nbsp;
+        <Button href='/mission/current/map/'>All Current Missions Map</Button>&nbsp;
+        <Button href='/mission/all/map/'>All Missions Map</Button>&nbsp;
+      </div>
+    )
+  }
+}
+
+class CompletedMissionList extends React.Component {
+  render () {
+    const missionRows = []
+    for (const missionIdx in this.props.missions) {
+      const mission = this.props.missions[missionIdx]
+      missionRows.push((
+        <MissionRow
+          key={mission.id}
+          mission={mission} />
+      ))
+    }
+    return (
+      <Table>
+        <thead>
+          <tr key='heading'>
+            <th colSpan={4} align='center'>Completed Missions</th>
+          </tr>
+          <tr key='labels'>
+            <th>Mission Name</th>
+            <th>Started</th>
+            <th>By</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          { missionRows }
+        </tbody>
+      </Table>)
+  }
+}
+CompletedMissionList.propTypes = {
+  missions: PropTypes.array.isRequired
+}
+
+export class MissionListPage extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      knownActiveMissions: [],
+      knownCompletedMissions: []
+    }
+  }
+
+  componentDidMount () {
+    this.updateData()
+    $.ajaxSetup({ timeout: 2500 })
+    this.timer = setInterval(() => this.updateData(), 10000)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timer)
+    this.timer = null
+  }
+
+  async updateData () {
+    const self = this
+    await $.get('/mission/list/', function (data) {
+      self.updateMissions(data.missions)
+    })
+  }
+
+  updateMissions (missions) {
+    const activeMissions = missions.filter(mission => !mission.closed)
+    const completeMissions = missions.filter(mission => mission.closed)
+    this.setState(function () {
+      return {
+        knownActiveMissions: activeMissions,
+        knownCompletedMissions: completeMissions
+      }
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <ActiveMissionList
+          missions={this.state.knownActiveMissions} />
+        <GeneralMissionButtons />
+        <CompletedMissionList
+          missions={this.state.knownCompletedMissions} />
+      </div>
+    )
+  }
+}
+
+export function createMissionList (elementId) {
+  const div = ReactDOM.createRoot(document.getElementById(elementId))
+  div.render(<MissionListPage />)
+}
+
+globalThis.createMissionList = createMissionList

map/static/.gitignore
@@ -1,6 +1,6 @@
 *.js
 *.js.*
-/*.css
+*.css
 /*.png
 /*.ttf
 /*.woff2

mission/templates/mission_list.html
@@ -4,69 +4,14 @@
     <head>
         <title>Mission List</title>
         <base href="/static/" />
-        <link rel="stylesheet" href="pretty.css">
-        <script type="module" src="pretty.js"></script>
+        <link rel="stylesheet" href="mission/list.css">
+        <script src="mission/list.js"></script>
     </head>
     <body style="height: 100%">
-        Current Missions:
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Mission Name</th>
-                    <th scope="col">Started</th>
-                    <th scope="col">By</th>
-                    <th scope="col">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-{% for mission in current_missions %}
-                <tr>
-                    <th scope="row">{{ mission.mission_name }}</th>
-                    <td>{{ mission.started }}</td>
-                    <td>{{ mission.creator }}</td>
-                    <td>
-                        <a class="btn btn-primary" href="/mission/{{ mission.pk }}/map/">Map</a>
-                        <a class="btn btn-primary" href="/mission/{{ mission.pk }}/details/">Details</a>
-                        <a class="btn btn-primary" href="/mission/{{ mission.pk }}/timeline/">Timeline</a>
-{% if mission.is_admin %}
-                        <a class="btn btn-danger" href="/mission/{{ mission.pk }}/close/">Close</a>
-{% endif %}
-                    </td>
-                </tr>
-            </tbody>
-{% endfor %}
-        </table>
-        <a class="btn btn-primary" href="/mission/new/">Start New Mission</a>
-        <a class="btn btn-primary" href="/mission/current/map/">All Current Missions Map</a>
-        <a class="btn btn-primary" href="/mission/all/map/">All Missions Map</a>
-        <br />
-        Completed Missions:
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Mission Name</th>
-                    <th scope="col">Started</th>
-                    <th scope="col">By</th>
-                    <th scope="col">Closed</th>
-                    <th scope="col">By</th>
-                    <th scope="col">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-{% for mission in previous_missions %}
-                <tr>
-                    <th scope="row">{{ mission.mission_name }}</th>
-                    <td>{{ mission.started }}</td>
-                    <td>{{ mission.creator }}</td>
-                    <td>{{ mission.closed }}</td>
-                    <td>{{ mission.closed_by }}</td>
-                    <td>
-                        <a class="btn btn-primary" href="/mission/{{ mission.pk }}/details/">Details</a>
-                    </td>
-                </tr>
-            </tbody>
-{% endfor %}
-        </table>
+        <div id="root"></div>
     </body>
+    <script>
+        createMissionList('root')
+    </script>
 </html>
 

mission/urls.py
@@ -18,5 +18,6 @@ urlpatterns = [
     re_path(r'^mission/(?P<mission_id>\d+)/assets/(?P<asset_id>\d+)/remove/$', views.mission_asset_remove, name='mission_assets_remove'),
     re_path(r'^mission/(?P<mission_id>\d+)/close/$', views.mission_close, name='mission_close'),
     re_path(r'^mission/new/$', views.mission_new, name='mission_new'),
+    re_path(r'^mission/list/$', views.mission_list_data, name='mission_list_data'),
     re_path(r'^$', views.mission_list, name='mission_list'),
 ]

commit 41e290d7179f990eeece352c5ed29b2e7cfcc899

mission: fix issues that pylint is reporting
fixes: 309f33b Convert the main page to be dynamically generated in js

mission/models.py
@@ -20,15 +20,18 @@ class Mission(models.Model):
     closed = models.DateTimeField(null=True, blank=True)
     closed_by = models.ForeignKey(get_user_model(), on_delete=models.PROTECT, related_name='closer%(app_label)s_%(class)s_related', null=True, blank=True)
 
-    def jsonObject(self, admin):
+    def as_object(self, admin):
+        """
+        Convert mission to an object that is suitable for returning via JsonResponse
+        """
         return {
             'id': self.pk,
             'name': self.mission_name,
             'description': self.mission_description,
             'started': self.started,
-            'creator': self.creator.username,
+            'creator': self.creator.username,  # pylint: disable=E1101
             'closed': self.closed,
-            'closed_by': self.closed_by.username if self.closed_by else None,
+            'closed_by': self.closed_by.username if self.closed_by else None,  # pylint: disable=E1101
             'admin': admin,
         }
 

mission/views.py
@@ -99,7 +99,7 @@ def mission_list_data(request):
     """
     user_missions = MissionUser.objects.filter(user=request.user)
     data = {
-        'missions': [ user_mission.mission.jsonObject(user_mission.is_admin()) for user_mission in user_missions ],
+        'missions': [user_mission.mission.as_object(user_mission.is_admin()) for user_mission in user_missions],
     }
     return JsonResponse(data)
 