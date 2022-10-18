f3fce4cf9d10680658b5041cba5a0752f7684935
Scott Parlane <sjp@canterburyairpatrol.org>
2022-10-18 22:44:05 +1300

    Convert the header on the timeline to be dynamically generated
    Reuse the the mission list row entry and allow it to be customized.
    Also, use this customization to change the way that closed missions are shown.
    
    This allows the mission header to be displayed consistently across pages.

---
+++ frontend/mission/header.js
@@ -0,0 +1,32 @@
+import { Table } from 'react-bootstrap'
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { MissionListRow } from './list'
+
+class MissionHeader extends React.Component {
+  render () {
+    return (
+      <Table>
+        <thead>
+          <tr>
+            <td>Misssion</td>
+            <td>Created</td>
+            <td>By</td>
+            <td>Closed</td>
+            <td>By</td>
+          </tr>
+        </thead>
+        <tbody>
+          <MissionListRow mission={this.props.mission} showButtons={false} showClosed={true} />
+        </tbody>
+      </Table>
+    )
+  }
+}
+MissionHeader.propTypes = {
+  mission: PropTypes.object.isRequired
+}
+
+export { MissionHeader }
