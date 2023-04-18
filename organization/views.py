canterbury-air-patrol/search-management-map

commit a44a74434ec2a5261c49e6225537bab9a83570cb

organization: present the UI for adding a user to an organization

organization/views.py
@@ -153,3 +153,16 @@ def organization_asset_modify(request, organization_member, asset):
         return JsonResponse(oa.as_object())
     else:
         return HttpResponseNotAllowed(['POST'])
+
+
+@login_required
+@organization_is_admin
+def organization_not_members(request, organization_member):
+    """
+    Get all users who aren't currently in this organization
+    """
+    organization = get_object_or_404(Organization, id=organization_id)
+    users = get_user_model().objects.exclude(pk__in=[OrganizationMember.objects.filter(removed__isnull=True, organization=organization_member.organization).values_list('user')])
+    return JsonResponse({
+        'users': [{'username': user.username, 'id': user.pk} for user in users]
+    })

frontend/organization/details.js
@@ -69,7 +69,7 @@ class OrganizationMemberList extends React.Component {
         <OrganizationMemberRow
           key={organizationMember.id}
           organization_member={organizationMember}
-          showButtons />
+          showButtons={this.props.showButtons} />
       ))
     }
     return (
@@ -92,7 +92,90 @@ class OrganizationMemberList extends React.Component {
   }
 }
 OrganizationMemberList.propTypes = {
-  organization_members: PropTypes.array.isRequired
+  organization_members: PropTypes.array.isRequired,
+  showButtons: PropTypes.bool
+}
+
+class OrganizationMemberAdd extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      userList: [],
+      userId: null
+    }
+
+    this.updateSelectedUser = this.updateSelectedUser.bind(this)
+    this.addOrganizationMember = this.addOrganizationMember.bind(this)
+  }
+
+  componentDidMount () {
+    $.ajaxSetup({ timeout: 2500 })
+    this.updateData()
+    this.timer = setInterval(() => this.updateData(), 10000)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timer)
+    this.timer = null
+  }
+
+  async updateData () {
+    const self = this
+    await $.getJSON(`/organization/${this.props.organizationId}/users/notmember/`, function (data) {
+      self.setState(function (oldState) {
+        const newState = {
+          userList: data.users
+        }
+        if (oldState.userId === null && data.users.length > 0) {
+          newState.userId = data.users[0].id
+        }
+        return newState
+      })
+    })
+  }
+
+  updateSelectedUser (event) {
+    const target = event.target
+    const value = target.value
+
+    this.setState({ memberId: value })
+  }
+
+  addOrganizationMember () {
+    const self = this
+    $.post(`/organization/${this.props.organizationId}/user/${this.state.userId}/`, { csrfmiddlewaretoken: this.props.csrftoken }, function () {
+      self.setState({ memberId: null })
+    })
+  }
+
+  render () {
+    const possibleMembers = []
+    for (const userIdx in this.state.userList) {
+      const user = this.state.userList[userIdx]
+      possibleMembers.push((<option key={user.id} value={user.id}>{user.username}</option>))
+    }
+    return (
+      <Table>
+        <thead>
+          <tr>
+            <td>Member</td>
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><select onChange={this.updateSelectedUser}>{possibleMembers}</select></td>
+            <td><Button onClick={this.addOrganizationMember}>Add</Button></td>
+          </tr>
+        </tbody>
+      </Table>
+    )
+  }
+}
+OrganizationMemberAdd.propTypes = {
+  organizationId: PropTypes.number.isRequired,
+  csrftoken: PropTypes.string.isRequired
 }
 
 class OrganizationAssetList extends React.Component {
@@ -247,32 +330,49 @@ class OrganizationDetailsPage extends React.Component {
   }
 
   render () {
-    return (
-      <div>
-        <Table>
-          <thead>
-            <tr>
-              <th>Organization Name</th>
-              <th>Created</th>
-              <th>Creator</th>
-              <th>Your Role</th>
-            </tr>
-          </thead>
-          <tbody>
-            <OrganizationListRow
-              organization={this.state.organizationDetails}
-              showButtons={false} />
-          </tbody>
-        </Table>
-        <OrganizationMemberList
+    const organizationSections = [(
+      <Table key='details'>
+        <thead>
+          <tr>
+            <th>Organization Name</th>
+            <th>Created</th>
+            <th>Creator</th>
+            <th>Your Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          <OrganizationListRow
+            organization={this.state.organizationDetails}
+            showButtons={false} />
+        </tbody>
+      </Table>
+    )]
+    organizationSections.push((
+      <OrganizationMemberList key='org_members'
           organization_members={this.state.organizationDetails.members}
-          showButtons={true} />
-        <OrganizationAssetList
-          organization_assets={this.state.organizationDetails.assets}
-          showButtons={true} />
-        <OrganizationAssetAdd
+          showButtons={this.state.organizationDetails.role === 'Admin'} />
+    ))
+    if (this.state.organizationDetails.role === 'Admin') {
+      organizationSections.push((
+        <OrganizationMemberAdd key='org_add_member'
           organizationId={this.props.organizationId}
           csrftoken={this.props.csrftoken} />
+      ))
+    }
+    organizationSections.push((
+      <OrganizationAssetList key='org_assets'
+        organization_assets={this.state.organizationDetails.assets}
+        showButtons={true} />
+    ))
+    organizationSections.push((
+      <OrganizationAssetAdd key='org_asset_add'
+        organizationId={this.props.organizationId}
+        csrftoken={this.props.csrftoken} />
+    ))
+
+    return (
+      <div>
+        { organizationSections }
       </div>
     )
   }

organization/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     re_path(r'^organization/list/mine/$', views.organization_list_mine, name='organization_list_mine'),
     re_path(r'^organization/(?P<organization_id>\d+)/$', views.organization_details, name='organization_details'),
     re_path(r'^organization/(?P<organization_id>\d+)/user/(?P<username>.*)/$', views.organization_user_modify, name='organization_user_modify'),
+    re_path(r'^organization/(?P<organization_id>\d+)/users/notmember/', views.organization_not_members, name='organization_not_members'),
     re_path(r'^organization/(?P<organization_id>\d+)/assets/$', views.organization_asset_list, name='organization_asset_list'),
     re_path(r'^organization/(?P<organization_id>\d+)/assets/(?P<asset_id>\d+)/$', views.organization_asset_modify, name='organization_asset_modify'),
 ]

commit 93abf6bc13ecdc7d559ffe4ee608e25fc8acd131

organization: fix users/notmember api
the organization is provided by the organization_member object that
@organization_is_admin provides.

Fixes: a44a744 organization: present the UI for adding a user to an organization

organization/views.py
@@ -161,7 +161,6 @@ def organization_not_members(request, organization_member):
     """
     Get all users who aren't currently in this organization
     """
-    organization = get_object_or_404(Organization, id=organization_id)
     users = get_user_model().objects.exclude(pk__in=[OrganizationMember.objects.filter(removed__isnull=True, organization=organization_member.organization).values_list('user')])
     return JsonResponse({
         'users': [{'username': user.username, 'id': user.pk} for user in users]