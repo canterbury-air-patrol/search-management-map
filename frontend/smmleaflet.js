29ed10f16b9fa0ee181c925829c552e74f4a0855
Scott Parlane <sjp@canterburyairpatrol.org>
2023-06-29 23:44:26 +1200

    LineAdder: Use the marker<->input bridge
    This makes changes in the input fields change the position of the markers
    
    Adds a callback to the MappedMarker class to allow the creator
    to do extra things, like in this case update the line when there
    is a change

--- frontend/smmleaflet.js
+++ frontend/smmleaflet.js
@@ -3,9 +3,10 @@ import L from 'leaflet'
 import { degreesToDM, DMToDegrees } from '@canterbury-air-patrol/deg-converter'
 
 class MappedMarker {
-  constructor (latInput, lngInput, pos) {
+  constructor (latInput, lngInput, pos, cb = null) {
     this.latInput = latInput
     this.lngInput = lngInput
+    this.cb = cb
     this.marker = L.marker(pos, {
       draggable: true,
       autoPan: true
@@ -22,12 +23,18 @@ class MappedMarker {
   updateMarkerLatLng () {
     const latLng = L.latLng(DMToDegrees(this.latInput.val()), DMToDegrees(this.lngInput.val()))
     this.marker.setLatLng(latLng)
+    if (this.cb !== null) {
+      this.cb()
+    }
   }
 
   updateTxtLatLng () {
     const markerCoords = this.marker.getLatLng()
     this.latInput.val(degreesToDM(markerCoords.lat, true))
     this.lngInput.val(degreesToDM(markerCoords.lng, false))
+    if (this.cb !== null) {
+      this.cb()
+    }
   }
 
   getMarker () {
