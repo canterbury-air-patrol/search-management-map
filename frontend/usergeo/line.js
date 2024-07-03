canterbury-air-patrol/search-management-map

commit b2685df470f70b0d1a692a663693498871ed8dc2

frontend: use the new usergeo DELETE API to remove objects
Instead of using the per-object type API to delete objects,
use the common usergeo DELETE API

frontend/usergeo/line.js
@@ -20,7 +20,10 @@ class SMMLine {
   }
 
   deleteCallback () {
-    $.get(`/data/userlines/${this.LineID}/delete/`)
+    $.ajax({
+      url: `/data/userlines/${this.LineID}/`,
+      method: 'DELETE'
+    })
   }
 
   createSearchCallback () {
@@ -60,7 +63,7 @@ class SMMLine {
         },
         {
           label: 'Details',
-          href: `/data/userlines/${this.LineID}/details/`,
+          href: `/data/usergeo/${this.LineID}/`,
           'btn-class': 'btn-light'
         }
       ]))

frontend/usergeo/poi.js
@@ -24,7 +24,10 @@ class SMMPOI {
   }
 
   deleteCallback () {
-    $.get(`/data/pois/${this.poiID}/delete/`)
+    $.ajax({
+      url: `/data/pois/${this.poiID}/`,
+      method: 'DELETE'
+    })
   }
 
   createSearchCallback () {
@@ -84,7 +87,7 @@ class SMMPOI {
         },
         {
           label: 'Details',
-          href: `/data/pois/${this.poiID}/details/`,
+          href: `/data/usergeo/${this.poiID}/`,
           'btn-class': 'btn-light'
         }
       ]))

frontend/usergeo/polygon.js
@@ -20,7 +20,10 @@ class SMMPolygon {
   }
 
   deleteCallback () {
-    $.get(`/data/userpolygons/${this.PolyID}/delete/`)
+    $.ajax({
+      url: `/data/usergeo/${this.PolyID}/`,
+      method: 'DELETE'
+    })
   }
 
   createSearchCallback () {
@@ -60,7 +63,7 @@ class SMMPolygon {
         },
         {
           label: 'Details',
-          href: `/data/userpolygons/${this.PolyID}/details/`,
+          href: `/data/usergeo/${this.PolyID}/`,
           'btn-class': 'btn-light'
         }
       ]))

commit 116ac28cdc0fe70eaf19d5d982b23dff3c935212

frontend: fix deleting of usergeo objects
Send the delete to the correct path, and include the CSRF token

Fixes: b2685df frontend: use the new usergeo DELETE API to remove objects

frontend/usergeo/line.js
@@ -10,6 +10,7 @@ class SMMLine {
     this.coords = line.geometry.coordinates
     this.LineLabel = line.properties.label
     this.LineID = line.properties.pk
+    this.setXHR = this.setXHR.bind(this)
     this.editCallback = this.editCallback.bind(this)
     this.deleteCallback = this.deleteCallback.bind(this)
     this.createSearchCallback = this.createSearchCallback.bind(this)
@@ -19,10 +20,15 @@ class SMMLine {
     L.LineAdder(this.parent.map, this.parent.missionId, this.parent.csrftoken, this.coords.map(x => L.latLng(x[1], x[0])), this.LineID, this.LineLabel)
   }
 
+  setXHR (xhr) {
+    xhr.setRequestHeader('X-CSRFToken', this.parent.csrftoken)
+  }
+
   deleteCallback () {
     $.ajax({
-      url: `/data/userlines/${this.LineID}/`,
-      method: 'DELETE'
+      url: `/data/usergeo/${this.LineID}/`,
+      type: 'DELETE',
+      beforeSend: this.setXHR
     })
   }
 

frontend/usergeo/poi.js
@@ -14,6 +14,7 @@ class SMMPOI {
     this.POILabel = poi.properties.label
     this.poiID = poi.properties.pk
     this.editCallback = this.editCallback.bind(this)
+    this.setXHR = this.setXHR.bind(this)
     this.deleteCallback = this.deleteCallback.bind(this)
     this.createSearchCallback = this.createSearchCallback.bind(this)
     this.calculateTDVCallback = this.calculateTDVCallback.bind(this)
@@ -23,10 +24,15 @@ class SMMPOI {
     L.POIAdder(this.parent.map, this.parent.missionId, this.parent.csrftoken, L.latLng(this.coords[1], this.coords[0]), this.poiID, this.POILabel)
   }
 
+  setXHR (xhr) {
+    xhr.setRequestHeader('X-CSRFToken', this.parent.csrftoken)
+  }
+
   deleteCallback () {
     $.ajax({
-      url: `/data/pois/${this.poiID}/`,
-      method: 'DELETE'
+      url: `/data/usergeo/${this.poiID}/`,
+      type: 'DELETE',
+      beforeSend: this.setXHR
     })
   }
 

frontend/usergeo/polygon.js
@@ -11,6 +11,7 @@ class SMMPolygon {
     this.PolyID = polygon.properties.pk
     this.coords = polygon.geometry.coordinates
     this.editCallback = this.editCallback.bind(this)
+    this.setXHR = this.setXHR.bind(this)
     this.deleteCallback = this.deleteCallback.bind(this)
     this.createSearchCallback = this.createSearchCallback.bind(this)
   }
@@ -19,10 +20,15 @@ class SMMPolygon {
     L.PolygonAdder(this.parent.map, this.parent.missionId, this.parent.csrftoken, this.coords[0].map(x => L.latLng(x[1], x[0])), this.PolyID, this.PolyLabel)
   }
 
+  setXHR (xhr) {
+    xhr.setRequestHeader('X-CSRFToken', this.parent.csrftoken)
+  }
+
   deleteCallback () {
     $.ajax({
       url: `/data/usergeo/${this.PolyID}/`,
-      method: 'DELETE'
+      type: 'DELETE',
+      beforeSend: this.setXHR
     })
   }
 