2b2a8979b26c8b9e9da80d38640352a1ca2c3f46
Scott Parlane <sjp@canterburyairpatrol.org>
2024-12-21 23:19:04 +1300

    Add typescript support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
 
@@ -7,5 +8,6 @@ export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
