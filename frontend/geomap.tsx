eab09efe3a545bb921152fb3a87b08b4199d1e7e
Scott Parlane <sjp@canterburyairpatrol.org>
2024-12-22 23:55:58 +1300

    convert geomap to typescript

--- frontend/geomap.tsx
+++ frontend/geomap.tsx
@@ -3,19 +3,25 @@ import 'bootstrap/dist/css/bootstrap.css'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import React from 'react'
-import PropTypes from 'prop-types'
 
 import markerIcon from 'leaflet/dist/images/marker-icon.png'
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
 import markerIconShadow from 'leaflet/dist/images/marker-shadow.png'
 import { MapContainer, Polyline, Polygon, Marker, TileLayer } from 'react-leaflet'
 
-import { coordinateToLatLng, mapCoordinates } from './geometry/details'
+import { mapCoordinates, coordinateToLatLng } from './geometry/details'
 
 import './geomap.css'
 
-class GeoJsonMap extends React.Component {
-  constructor(props) {
+interface GeoJsonMapProps {
+  geometry: {
+    type: string
+    coordinates: [number, number] | [number, number][] | [number, number][][]
+  }
+}
+
+class GeoJsonMap extends React.Component<GeoJsonMapProps, never> {
+  constructor(props: GeoJsonMapProps) {
     super(props)
 
     L.Icon.Default.prototype.options.iconUrl = markerIcon
@@ -31,20 +37,20 @@ class GeoJsonMap extends React.Component {
     switch (geometry.type) {
       case 'LineString':
         {
-          const coordinates = mapCoordinates(geometry.coordinates)
+          const coordinates = mapCoordinates(geometry.coordinates as [number, number][])
           firstPoint = coordinates[0]
           objects.push(<Polyline key="linestring" positions={coordinates} />)
         }
         break
       case 'Polygon':
         {
-          const coordinates = mapCoordinates(geometry.coordinates[0])
+          const coordinates = mapCoordinates((geometry.coordinates as [number, number][][])[0])
           firstPoint = coordinates[0]
           objects.push(<Polygon key="polygon" positions={coordinates} />)
         }
         break
       case 'Point':
-        firstPoint = coordinateToLatLng(geometry.coordinates)
+        firstPoint = coordinateToLatLng(geometry.coordinates as [number, number])
         objects.push(<Marker key="point" position={firstPoint} />)
         break
       default:
@@ -66,9 +72,6 @@ class GeoJsonMap extends React.Component {
       </MapContainer>
     )
   }
-}
-GeoJsonMap.propTypes = {
-  geometry: PropTypes.object.isRequired
 }
 
 export { GeoJsonMap }
