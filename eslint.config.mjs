10936f5e95059291493890f75594a34bed2714a2
Scott Parlane <sjp@canterburyairpatrol.org>
2025-05-10 20:15:05 +1200

    Tell eslint to detect the react version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,4 +10,11 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
+  }
 ];
