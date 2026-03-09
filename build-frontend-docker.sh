da7c2b076d46a030435cec608b366a1367cd0e97
Scott Parlane <sjp@canterburyairpatrol.org>
2026-03-09 23:16:19 +1300

    Use node 24 to build the frontend

--- build-frontend-docker.sh
+++ build-frontend-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
 
-docker run --mount type=bind,source=$(pwd),target=/usr/src node:20-slim /bin/bash -c "cd /usr/src; USERID=$(id -u) GROUPID=$(id -g) ./build-frontend.sh"
+docker run --mount type=bind,source=$(pwd),target=/usr/src node:24-slim /bin/bash -c "cd /usr/src; USERID=$(id -u) GROUPID=$(id -g) ./build-frontend.sh"
